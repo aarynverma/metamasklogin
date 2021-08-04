47398871d5acc9a2173130f68ee62274ff48595d
aarynverma <18100BTCSE02667@svvvindore.onmicrosoft.com>
2021-08-04 14:13:48 +0530

    create metamask connection in one click

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { ChakraProvider, useDisclosure } from "@chakra-ui/react";
+import Layout from "./components/Layout";
+import ConnectButton from "./components/ConnectButton";
+import theme from "./theme";
+import AccountModal from "./components/AccountModal";
+import "@fontsource/inter";
+
+
+export default function App() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    // lets us use Chakra UI syntax across our app:
+    <ChakraProvider theme={theme}>
+        <Layout>
+        <p style={{ color: "white" }}>Click the Below Button : </p>
+        <ConnectButton handleOpenModal={onOpen} />
+        {/* Our Account modal will handle open state & closing */}
+        <AccountModal isOpen={isOpen} onClose={onClose} />
+        </Layout>
+    </ChakraProvider>
+  )
+}
